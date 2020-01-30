506b0e09ea1d39e3085f1004031d7f63b2fb8d4f
Vinod <vinod@vinod.co>
2020-01-30 20:31:05 +0530

    🇮🇳

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ var server = jsonServer.create()
 var router = jsonServer.router(require('./db.js')())
 var middlewares = jsonServer.defaults()
 
+// middleware for all incoming requests to handle CORS
+server.use((req, resp, next) => {
+    resp.set('Access-Control-Allow-Origin', '*');
+    resp.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT');
+    resp.set('Access-Control-Allow-Headers', 'Content-Type,Accept,Authorization');
+    next();
+});
+
+// a handler function for POST requests for the url '/customers'
+server.options('/*', (req, resp) => {
+    resp.end();
+});
+
+server.get('/customers', (req, resp, next) => {
+    let auth = req.headers.authorization;
+    if (!auth) {
+        resp.status(401).json('Authorization header is missing');
+        return;
+    }
+
+    let [bearer, token] = auth.split(' ');
+    if (bearer && bearer === 'Bearer') {
+        try {
+            let user = jwt.verify(token, SECRET_KEY);
+            req.params.id = user.id;
+            next()
+        }
+        catch (e) {
+            console.log(e);
+            resp.status(403).json('Authorization token is not valid');
+        }
+    }
+    else {
+        resp.status(403).json('Authorization token is not valid');
+    }
+});
+
 server.use(middlewares)
 server.use(router)
 server.listen(3000, function () {
