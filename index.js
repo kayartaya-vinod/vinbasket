0e0c143dcd60f1c1c93399a2f2bc204eacccdc6b
Vinod <vinod@vinod.co>
2020-01-30 13:53:00 +0530

    one more push

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const app = express();
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const fs = require('fs');
+let data = fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8');
+data = JSON.parse(data);
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 80;
 
+server.use(middlewares);
+server.use(router);
 
-app.get('/', (req, resp)=>resp.end('Hello, world!'));
-
-app.listen(port);
+server.listen(port, ()=>{
+    console.warn('VINOD:: deployed on port', port);
+});
